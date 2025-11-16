b93b25eff9ba9c2f528684719070b471c6b59e9d
Advancedboy <legobuilder0819@gmail.com>
2025-11-16 22:46:30 +0300

    Added tags loading

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -39,6 +39,24 @@ export const getOne = async (req, res) => {
   }
 };
 
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -7,6 +7,7 @@ import { postCreateValidation } from "../validations.js";
 const router = express.Router();
 
 router.get("/", PostController.getAll);
+router.get("/tags", PostController.getLastTags);
 router.get("/:id", PostController.getOne);
 router.post(
   "/",
